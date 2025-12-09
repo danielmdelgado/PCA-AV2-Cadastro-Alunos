186e3b1a3626fc30433639f89afc3fe99c6926cb
Daniel Moraes Delgado <danielmdelgado11@gmail.com>
2025-12-09 18:30:40 -0300

    update file_manager

--- functions/file_manager.py
+++ functions/file_manager.py
@@ -7,7 +7,7 @@ def carregar_csv():
     Lê o arquivo alunos.csv e retorna um DataFrame.
     Se o arquivo não existir, cria um DataFrame vazio.
     """
-    if os.path.exists("alunos.csv")
+    if os.path.exists("alunos.csv"):
         df = pd.read_csv("alunos.csv")
     else:
         df = pd.DataFrame(columns=["matricula", "nome", "rua", "numero", "bairro", "cidade", "uf", "telefone", "email"])
